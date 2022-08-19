2ad607d79233f5651b36a1b5cae251de79e89978
Gustavo Akyama <gustavoakyamamga01@gmail.com>
2022-08-19 18:02:58 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,32 @@
+package boardgame;
+
+public class Position {
+
+    private Integer row;
+    private Integer column;
+
+    public Position(Integer row, Integer column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public Position() {
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setValues(int row, int column) {
+
+    }
+
+    @Override
+    public String toString(){
+        return row + ", " + column;
+    }
+}
